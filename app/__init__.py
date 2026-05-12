ee6a5a2536b358bbfcc7ea258be378870bf8a7a4
33fish <zzhfisher@outlook.com>
2026-05-12 19:13:22 +0800

    Refactor app initialization and register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,20 @@
-"""
-This file initializes the Flask application and sets up the database connection using SQLAlchemy.
-It also imports the necessary routes and models to be used in the application.
-"""
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 
 db = SQLAlchemy()
-csrf = CSRFProtect()   # Enable CSRF protection
+csrf = CSRFProtect()
 
-# Function to create and configure the Flask application
+
 def create_app():
-    # Create a new Flask application instance
     app = Flask(__name__)
 
-    # Configure the application with necessary settings
     app.config['SECRET_KEY'] = "mysecretkey"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///NewsSentimentDB.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize the database with the application
     db.init_app(app)
-    csrf.init_app(app)   # Attach CSRF protection to the app
+    csrf.init_app(app)
 
     # Register models
     from app.models.user import User
@@ -33,15 +25,13 @@ def create_app():
     from app.routes.users import users_bp
     app.register_blueprint(users_bp)
 
+    from app.routes.competitor import competitor_bp
+    app.register_blueprint(competitor_bp)
+
     from app.routes.comments import comments_bp
     app.register_blueprint(comments_bp)
 
     from app.routes.main import main_bp
     app.register_blueprint(main_bp)
-
-    from app.routes.competitor import competitor_bp
-    app.register_blueprint(competitor_bp)
-
-
 
     return app
