16a6527f4fb7e13567162b22847b4e43ce495917
33fish <zzhfisher@outlook.com>
2026-05-12 19:04:52 +0800

    Add nullable=False to likes column in Comment model

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,18 +1,24 @@
 from app import db
 from datetime import datetime, timezone
 
+
 class Comment(db.Model):
     __tablename__ = "comments"
 
-    id=db.Column(db.Integer, primary_key=True)
-    article_id=db.Column(db.Integer, db.ForeignKey("articles.id"), nullable=False)
-    user_id= db.Column(db.Integer, db.ForeignKey("users.id"),    nullable=False)
-    parent_id=db.Column(db.Integer, db.ForeignKey("comments.id"), nullable=True)  # None = 顶级评论
-    content = db.Column(db.Text, nullable=False)
-    likes = db.Column(db.Integer, default=0)
+    id         = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey("articles.id"), nullable=False)
+    user_id    = db.Column(db.Integer, db.ForeignKey("users.id"),    nullable=False)
+    parent_id  = db.Column(db.Integer, db.ForeignKey("comments.id"), nullable=True)
+    content    = db.Column(db.Text, nullable=False)
+    likes      = db.Column(db.Integer, default=0, nullable=False)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
-    author = db.relationship("User", backref="comments", lazy="joined")
-    replies = db.relationship("Comment",
-                              backref=db.backref("parent", remote_side=[id]),
-                              lazy="select")
+    author  = db.relationship("User", backref="comments", lazy="joined")
+    replies = db.relationship(
+        "Comment",
+        backref=db.backref("parent", remote_side=[id]),
+        lazy="select"
+    )
+
+    def __repr__(self):
+        return f"<Comment {self.id} by User {self.user_id}>"
